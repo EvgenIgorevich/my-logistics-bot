bdb4722af5b60df1eb753c7bc98966da669025f2
EvgenIgorevich <jekson544@mail.ru>
2026-09-24 16:32:55 +0300

    Change Telegram token in main.py
    Updated the Telegram token for the bot.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import re
 import os
 
 # --- 1. НАСТРОЙКА ---
-TELEGRAM_TOKEN = "8734739136:AAEpqPB0rMzobwTgF1qX1rESgAovkX9oTCI"
+TELEGRAM_TOKEN = "8734739136:AAFPitxnE8fvzr_wu1BpQJeZD8FG_vFz9Bo"
 # Сюда вставьте ID Группы, который вы найдете (например -1001234567890)
 TG_CHAT_ID = os.environ.get("TG_CHAT_ID") 
 
